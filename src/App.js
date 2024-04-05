194ec5f98be0abce127419a74368803c2cf16c28
Codeplus12 <mohitbaghel001@outlook.com>
2024-04-05 09:45:09 +0530

    first phase

--- src/App.js
+++ src/App.js
@@ -11,9 +11,14 @@ function App() {
     <Col className='bi'>
     <Container fluid>
       <Row>
-      <Col md={3}></Col>
-      <Col md={6} className='fs text-dark ff'>Alexandrina
-Kennedy</Col>
+      <Col md={3} className='ps-5 pt-5'>
+        <img src="img/alexandrina-photo.jpg" alt="" className='h200 w60 ms-md-5 ms-0 mt-5'/>
+        <img src="img/alexandrina-certified.png" className='h100 w10 p'/>
+      </Col>
+      <Col md={6} className='fs text-dark ff ps-5 fw5'>
+        <p className='p-0 m-0'>Alexandrina
+Kennedy</p>
+</Col>
       <Col md={3}></Col>
       </Row>
     </Container>
